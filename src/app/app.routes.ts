0f69e35a0d189c00b963f439c1385562b0631733
DayanaCueva18 <dannacorrea665@gmail.com>
2025-05-17 15:10:40 -0500

    form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,8 @@ import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 
 export const routes: Routes = [
-  { path: '', component: FormComponent }
+  { path: '', component: FormComponent },
+
+  {path: 'form', component: FormComponent},
+
 ];
