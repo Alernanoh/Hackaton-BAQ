fde9efc0bc5d4a65a159c487eb0b47a4dc3551d3
Nano <alhernano@gmail.com>
2025-05-17 15:52:45 -0500

    Changed the names of the navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
-import { FormComponent } from './form/form.component';
 
 export const routes: Routes = [
-  { path: '', component: FormComponent },
 
-  {path: 'form', component: FormComponent},
 
 ];
