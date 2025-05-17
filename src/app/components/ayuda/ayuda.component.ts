37516d476aa1bc530a12a2180a8409ce92f376f0
Andrés Pallango Tapia <andy.pal001@gmail.com>
2025-05-17 17:50:34 -0500

    Revert "Merge branch 'master' of https://github.com/Alernanoh/Hackaton-BAQ"
    This reverts commit 299b195ee16e12dffb6ce0d022b3095d2dc31b58, reversing
    changes made to f3ab8c02706cf3dbc624ccad42690b83c6002f12.

--- src/app/components/ayuda/ayuda.component.ts
+++ src/app/components/ayuda/ayuda.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
-import { DonacionCarritoComponent } from '../donacion-carrito/donacion-carrito.component';
-import { CommonModule } from '@angular/common';
-import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-ayuda',
-  imports: [DonacionCarritoComponent, CommonModule,
-    AppComponent
-  ],
+  imports: [],
   templateUrl: './ayuda.component.html',
   styleUrl: './ayuda.component.css'
 })
 export class AyudaComponent {
-  seccionActiva: string = 'productos'; // Sección inicial
 
-  cambiarSeccion(seccion: string): void {
-    this.seccionActiva = seccion;
-  }
 }
