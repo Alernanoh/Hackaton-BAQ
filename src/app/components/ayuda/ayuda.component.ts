dd0c973b14f8ce3c89c5c8f2a269acac0fae6bac
Nano <alhernano@gmail.com>
2025-05-17 17:57:12 -0500

    Added some changes

--- src/app/components/ayuda/ayuda.component.ts
+++ src/app/components/ayuda/ayuda.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { DonacionCarritoComponent } from '../donacion-carrito/donacion-carrito.component';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from '../../app.component';
+import { DonaPropositoComponent } from '../dona-proposito/dona-proposito.component';
 
 @Component({
   selector: 'app-ayuda',
-  imports: [DonacionCarritoComponent, CommonModule,
-    AppComponent
+  imports: [DonacionCarritoComponent,
+    CommonModule,
+    DonaPropositoComponent
   ],
   templateUrl: './ayuda.component.html',
   styleUrl: './ayuda.component.css'
