ae754c2d5a5f53d4f59a4f335feb0f68ac3265de
Andrés Pallango Tapia <andy.pal001@gmail.com>
2025-05-18 13:40:57 -0500

    Buy Cart Update

--- src/app/components/donacion-carrito/donacion-carrito.component.ts
+++ src/app/components/donacion-carrito/donacion-carrito.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormComponent } from "../form/form.component";
 
 @Component({
   selector: 'app-donacion-carrito',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormComponent],
   templateUrl: './donacion-carrito.component.html',
   styleUrls: ['./donacion-carrito.component.css']
 })
 export class DonacionCarritoComponent {
   @Input() carrito: any[] = [];
   @Output() quitarProducto = new EventEmitter<any>();
+
+  mostrarModalPago = false;
 
   get subtotal(): number {
     return this.carrito.reduce((total, item) => total + item.precio * item.cantidad, 0);
@@ -22,5 +25,15 @@ export class DonacionCarritoComponent {
 
   get total(): number {
     return +(this.subtotal + this.cargosPasarela).toFixed(2);
+  }
+
+  abrirPagoCarrito() {
+    this.mostrarModalPago = true;
+    document.body.style.overflow = 'hidden';
+  }
+
+  cerrarPagoCarrito() {
+    this.mostrarModalPago = false;
+    document.body.style.overflow = '';
   }
 }
